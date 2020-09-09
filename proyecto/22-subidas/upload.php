72fd3ce31110dd0d6be268cc2250607a970c04f7
Jesus Posada <jesus.posada@wiedii.co>
2020-09-09 11:48:31 -0500

    Se finaliza seccion 24 del curso

--- proyecto/22-subidas/upload.php
+++ proyecto/22-subidas/upload.php
@@ -1,6 +1,20 @@
 <?php
 $archivo = $_FILES['archivo'];
-var_dump($archivo);
-die();
+$nombre = $archivo['name'];
+$tipo = $archivo['type'];
+// Confirma que el archivo tenga la extencion deseada
+if ($tipo == "image/jpg" || $tipo == "image/jpeg" || $tipo == "image/png" || $tipo == "image/gif") {
+    // Crea carpeta si no existe
+    if (!is_dir('images')) {
+        mkdir('images', 0777);
+    }
+    header("Refresh: 3; URL=index.php");
+    move_uploaded_file($archivo['tmp_name'], 'images/'.$nombre);
+    echo '<strong>Su imagen se subio correctamente :) </strong>';
 
+}else{
+    // Redireccion automatica luego de 3 segundos
+    header("Refresh: 3; URL=index.php");
+    echo '<h1>Solo se permiten imagenes de extensiones jpg, jpeg, png o gif</h1>';
+}
 ?>
