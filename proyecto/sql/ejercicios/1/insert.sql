56cd2ec9482c34361ea90cfbbb80ccf7f302f09c
Jesus Posada <jesus.posada@wiedii.co>
2020-09-11 11:04:37 -0500

    Se finaliza seccion de 30 ejercicios SQL

--- proyecto/sql/ejercicios/1/insert.sql
+++ proyecto/sql/ejercicios/1/insert.sql
@@ -6,6 +6,7 @@ VALUES ('Renaul Clio', 'Renault', 12000, 13),
        ('Porche Cayene','Porche',65000,5), 
        ('Lambo Aventador','Lamborgini',170000,2), 
        ('Ferrari Spider','Ferrari',24500,80);
+       ('Ferrari Tributo','Ferrari',27500,12);
 # GRUPOS
 INSERT INTO grupos (nombre, ciudad)
 VALUES ('Vendedores A','Cucuta'),
@@ -27,12 +28,12 @@ INSERT INTO vendedores (grupo_id, jefe, nombre, apellidos, cargo, fecha_alta, su
 VALUES (1,NULL,'Jesus David','Posada Escobar','Responsable de Tienda',CURDATE(),185000,4),
        (1,1,'Elliot','Posada Carvajal','Ayudante en Tienda',CURDATE(),37000,3), 
        (2,NULL,'Daniel','Habif','Responsable de Tienda',CURDATE(),70000,5), 
-       (2,3,'Victor Manuel','Sambrano Arenas','Ayudande en Tienda',CURDATE(),55000,6), 
+       (2,3,'Victor Manuel','Sambrano Arenas','Ayudante en Tienda',CURDATE(),55000,6), 
        (3,NULL,'Javier Eduardo','Carvajal Escobar','Mecanico Jefe',CURDATE(),10000,2), 
        (4,NULL,'Richard','Acosta','Vendedor Recambios',CURDATE(),14500,8), 
        (5,NULL,'Johanna Karina','Carvajal Ortega','Vendedor Experto',CURDATE(),10000,2), 
        (6,NULL,'Billy','Graham','Ejecutivo de Cuentas',CURDATE(),80000,1), 
-       (6,8,'Saulo','De Tarso','Ayudante de Tienda',CURDATE(),10000,10);
+       (6,8,'Saulo','De Tarso','Ayudante en Tienda',CURDATE(),10000,10);
 
 # CLIENTES
 INSERT INTO clientes (vendedor_id, nombre, ciudad, gastado, fecha)
@@ -47,7 +48,10 @@ VALUES (1, 'Constructora Monape', 'Cucuta', 24000, CURDATE()),
 INSERT INTO encargos (cliente_id, coche_id, cantidad, fecha)
 VALUES (1,1,2,CURDATE()),
        (2,2,4,CURDATE()),
+       (2,1,4,CURDATE()),
+       (2,2,2,CURDATE()),
        (3,3,1,CURDATE()),
+       (3,2,1,CURDATE()),
        (4,3,3,CURDATE()),
        (5,5,1,CURDATE()),
        (6,6,1,CURDATE());
