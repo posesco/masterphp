99cd05ac370faffe58c396ec2d12228ec2b98d69
Jesus Posada <jesus.posada@wiedii.co>
2020-09-14 07:41:59 -0500

    Se implementea la hoja de estilos

--- proyecto/04_blog/index.php
+++ proyecto/04_blog/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Curso PHP :: Video Juegos</title>
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
 </head>
     <body>
         <!-- CABECERA -->
