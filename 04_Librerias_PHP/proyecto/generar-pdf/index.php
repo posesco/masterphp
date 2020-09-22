7fd2b782da7cae492af0fb34b051bf9482be4e95
Jesus Posada <jesus.posada@wiedii.co>
2020-09-21 19:56:35 -0500

    Se implemente HTML2PDF

--- 04_Librerias_PHP/proyecto/generar-pdf/index.php
+++ 04_Librerias_PHP/proyecto/generar-pdf/index.php
@@ -1,16 +1,12 @@
 <?php 
 require '../vendor/autoload.php';
 use Spipu\Html2Pdf\Html2Pdf;
-$html2pdf = new Html2Pdf();
-$html  = '<h1>Hola mundo desde PHP hacia creador de PDFs</h1>';
-$html .= '<p>Parrafo ..</p>';
-$html .= '<p>Parrafo ..</p>';
-$html .= '<p>Parrafo ..</p>';
-$html .= '<p>Parrafo ..</p>';
-$html .= '<p>Parrafo ..</p>';
-$html .= '<p>Parrafo ..</p>';
-$html .= '<p>Parrafo ..</p>';
-$html2pdf->writeHTML($html);
-$html2pdf->output();
 
+$html2pdf = new Html2Pdf();
+// Recoger la vista para imprimir
+ob_start();
+require_once 'pdf_para_generar.php';
+$html =ob_get_clean();
+$html2pdf->writeHTML($html);
+$html2pdf->Output('ejemplo.pdf');
 ?>
---
+++ 04_Librerias_PHP/proyecto/generar-pdf/pdf_para_generar.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generador PDF</title>
+    <style type="text/css">
+        body {
+            background: url(../img/cubes.png) #F4F4F4;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+        h1 {
+            font-size: 32px;
+            color: #444;
+            letter-spacing: 1px;
+            text-shadow: 1px 1px 2px #ccc;
+        }
+
+        h2 {
+            font-size: 20px;
+            color: #2979CD;
+            text-shadow: 1px 1px 2px #ccc;
+            letter-spacing: 1px;
+            margin-top: 25px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Hola mundo desde PHP hacia creador de PDFs</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+        cum quae fugiat sit ad illum dicta fugit! Earum blanditiis
+        expedita, cumque ipsum dolorem voluptate? Consequatur dolorem sequi maxime ducimus dolor!
+    </p>
+    <h2>Hola mundo desde PHP hacia creador de PDFs</h2>
+</body>
+
+</html>
